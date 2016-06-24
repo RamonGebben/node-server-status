57e4f53bbc39cc64259422ee47f25a29920ebd90
jll <jll@spacemetric.com>
2016-06-24 17:38:15 +0200

    Checks for success/failure status of the remote server. Next step is to have some nice html and css around.

--- public/app.js
+++ public/app.js
@@ -3,17 +3,22 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     var isUp = document.getElementById('isUp');
 
-    var statusUrl = "http://192.168.51.191:8080/servlets/soap?REQUEST=IsAlive";
+    var statusUrl = "/status";
 
     fetch(statusUrl)
         .then(function (response) {
             console.log(response.status);
-            console.log(true);
             // return response.blob();
             // response.text().then(function (text) {
             // console.log(ok);
             // do something with the text response    
             // });
+            if(response.ok){
+                console.log("it is up");
+            }
+            else{
+                console.log("it is down");
+            }
         })
         .catch(function (error) {
             console.log('There has been a problem with your fetch operation: ' + error.message);
